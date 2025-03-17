270e19a9dd33dd1518abb058b5dc5fc0446d9e1b
Carter Smith <cartergsmith11@gmail.com>
2025-03-17 00:29:12 -0400

    Fix history page and service worker

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ self.addEventListener("install", (event) => {
             return cache.addAll([
                 "/",
                 "/index.html",
+                "/history.html",
                 "/manifest.json",
                 "/icons/icon-192x192.png",
                 "/icons/icon-512x512.png"
@@ -16,6 +17,21 @@ self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
+        })
+    );
+});
+
+// Force update on new Service Worker
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== "budget-tracker-v1") {
+                        return caches.delete(cache);
+                    }
+                })
+            );
         })
     );
 });
